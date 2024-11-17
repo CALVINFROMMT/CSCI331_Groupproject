859d5f1479c31fd0f6434d4f22295e7ff74ad404
CALVINFROMMT <jessica.m.calvin@gmail.com>
2024-11-17 16:12:07 -0700

    test

--- php/admin_dashboard.php
+++ php/admin_dashboard.php
@@ -1,9 +1,19 @@
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Listing</title>
+   
+</head>
+<body>
+    <h1>ADMIN DASHBOARD</h1>
+   <?php
 session_start();
 
 // Check if the user is logged in and has admin privileges
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: login_user.php");
     exit();
 }
 
